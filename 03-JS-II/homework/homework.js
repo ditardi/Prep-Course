0307b83ba0d3acb0cfa701c78d93ee18c0272ced
tardi <ditardi@gmail.com>
2022-08-06 18:37:56 -0300

    homework html y pendientes

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -178,14 +178,11 @@ function esPrimo(numero) {
 if (numero===0 || numero ===1) {
   return false
 }
-for (var i=2, i<numero, i++) {
+for (var i=2; i<numero; i++)
   if (numero%i===0) {
     return false;
   }
-  else {
     return true;
-  }
-}
 
 }
 
